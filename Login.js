f1ef9899364e6f528b1efad80e07a7556709a0f0
jwm5909 <jmclaughlin@jjkeller.com>
2017-08-10 13:40:29 -0400

    added image assets and image to login page

--- Login.js
+++ Login.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
-import { AppRegistry, Text, View, StyleSheet } from 'react-native';
+import { AppRegistry, Text, View, Iamge, StyleSheet } from 'react-native';
 
 export default class Login extends React.Component {
     render() {
         return (
         <View style={styles.container}>
+	        <Image style= {styles.logo}
+	        source={require('image!Octocat')} />
         </View>
         );
     }
@@ -15,6 +17,10 @@ var styles = StyleSheet.create({
 	container: {
 		backgroundColor: '#333',
 		flex: 1
+	},
+	logo: {
+		width:66,
+		height: 55
 	}
 })
 
