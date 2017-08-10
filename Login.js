29b66ec266a3028b4ee138b01c5e414304288166
jwm5909 <jmclaughlin@jjkeller.com>
2017-08-10 14:40:38 -0400

    Added login fields and button

--- Login.js
+++ Login.js
@@ -1,13 +1,23 @@
 import React from 'react';
 
-import { AppRegistry, Text, View, Iamge, StyleSheet } from 'react-native';
+import { AppRegistry, Text, TextInput, TouchableHighlight, View, Image, StyleSheet } from 'react-native';
 
 export default class Login extends React.Component {
     render() {
         return (
         <View style={styles.container}>
 	        <Image style= {styles.logo}
-	        source={require('image!Octocat')} />
+	        	source={{ uri: "Octocat", isStatic: true }} />
+	        <Text style={styles.heading}>Jamie's Totally Rad {"\n"}Github Browser</Text>
+	        <TextInput style={styles.input}
+	        	placeholder="Github Username" />
+	        <TextInput style={styles.input}
+	        	placeholder="Github Password" secureTextEntry="true"/>
+	        <TouchableHighlight style={styles.button}>
+	        	<Text style={styles.buttonText}>
+	        		Login
+	        	</Text>
+        	</ TouchableHighlight>
         </View>
         );
     }
@@ -15,12 +25,41 @@ export default class Login extends React.Component {
 
 var styles = StyleSheet.create({
 	container: {
-		backgroundColor: '#333',
-		flex: 1
+		backgroundColor: '#F5FCFF',
+		flex: 1,
+		paddingTop:40,
+		alignItems: 'center',
+		padding: 12
 	},
 	logo: {
 		width:66,
 		height: 55
+	},
+	heading: {
+		fontSize: 25,
+		marginTop: 10
+	},
+	input: {
+		height:50,
+		width: 350,
+		marginTop: 10,
+		padding: 4,
+		fontSize: 18,
+		borderWidth: 1,
+		borderColor: '#48bbec'
+	},
+	button: {
+		height: 50,
+		backgroundColor: '#48BBEC',
+		alignSelf: 'stretch',
+		marginTop:10,
+		justifyContent: 'center'
+
+	},
+	buttonText: {
+		fontSize: 22,
+		color: '#FFF',
+		alignSelf: 'center'
 	}
 })
 
